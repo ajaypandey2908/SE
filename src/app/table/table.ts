e8f8a6c0883b7c27f41427d1547dcbc230ef86e5
Ajay Pandey <ajaypandey2908@gmail.com>
2026-04-28 18:55:20 +0530

    updated files

--- src/app/table/table.ts
+++ src/app/table/table.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [SelectModule, InputGroupModule, InputGroupAddonModule, InputTextModule, FormsModule],
   templateUrl: './table.html',
   styleUrl: './table.scss',
 })
