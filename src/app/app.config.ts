aa758205b50741ad49df95d4c2cf4e0ecd6e5653
Ajay Pandey <ajaypandey2908@gmail.com>
2026-05-01 16:50:54 +0530

    Initial commit - loginPageForm

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-// import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient,withInterceptorsFromDi } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
